9a0c5cafe72786fe80293e7a2ecb0aa78339998c
Kimjisungs <kjsqwe9@gmail.com>
2020-02-01 00:10:37 +0900

    make wrapper component

---
+++ src/Components/Wrapper.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 0 10px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 1280px;
+`;
+
+export default ({ children }) => {
+  return (
+    <Wrapper>
+      <Container>{children}</Container>
+    </Wrapper>
+  );
+};

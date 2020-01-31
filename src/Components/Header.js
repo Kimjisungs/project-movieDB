1fd4c80963ca42cfc7f1db7377b42d1eac9dfdf9
Kimjisungs <kjsqwe9@gmail.com>
2020-01-31 18:09:49 +0900

    Feat: settings logo on header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-export default () => {};
+const Header = styled.header``;
+
+const Logo = styled.h1``;
+
+export default () => (
+  <Header>
+    <Logo>
+      <Link to="/">
+        <img src="https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg" />
+      </Link>
+    </Logo>
+  </Header>
+);

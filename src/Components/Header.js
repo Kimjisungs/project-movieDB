ca283116735c21a35ea3df7f5e032c8b4202e023
Kimjisungs <kjsqwe9@gmail.com>
2020-01-31 18:13:30 +0900

    Style: change logo, header style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Header = styled.header``;
+const Header = styled.header`
+  padding: 10px;
+`;
 
-const Logo = styled.h1``;
+const Logo = styled.h1`
+  width: 100px;
+  a {
+    display: block;
+  }
+`;
 
 export default () => (
   <Header>

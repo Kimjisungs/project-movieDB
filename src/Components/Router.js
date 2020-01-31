18c6db2fedfbc846033ec06ce9e7437e1ff71be0
Kimjisungs <kjsqwe9@gmail.com>
2020-02-01 00:13:02 +0900

    Feat: add Header, Wrapper, Route on route.js

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "Components/Header";
+import Wrapper from "Components/Wrapper";
 
 import Home from "Routes/Home";
 
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" component={Home} />
-    </Switch>
+    <Header />
+    <Wrapper>
+      <Switch>
+        <Route path="/home" component={Home} />
+      </Switch>
+    </Wrapper>
   </Router>
 );

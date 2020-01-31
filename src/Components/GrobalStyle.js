6fb57b1f1d416255d001adc2caf255e3b9346363
Kimjisungs <kjsqwe9@gmail.com>
2020-01-31 17:21:12 +0900

    Style: setting reset style

--- src/Components/GrobalStyle.js
+++ src/Components/GrobalStyle.js
@@ -1,5 +1,21 @@
 import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 
-const GlobalStyle = createGlobalStyle``;
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  a {
+    text-decoration:none;
+    color: inherit;
+  }
+  * {
+    box-sizing:border-box;
+  }
+  body{
+    font-family: Georgia, "Times New Roman", serif;
+    font-size: 12px;
+    color: white;
+    height: 100vh;
+  }
+`;
 
 export default GlobalStyle;

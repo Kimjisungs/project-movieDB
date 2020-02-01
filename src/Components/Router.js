508710474b7e1057ef4e31102d756a2481b46bc9
Kimjisungs <kjsqwe9@gmail.com>
2020-02-01 22:32:04 +0900

    Feat: redirect develop

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
 import Header from "Components/Header";
 import Wrapper from "Components/Wrapper";
 
@@ -11,6 +16,7 @@ export default () => (
     <Wrapper>
       <Switch>
         <Route path="/home" component={Home} />
+        <Redirect from="*" to="/home" />
       </Switch>
     </Wrapper>
   </Router>

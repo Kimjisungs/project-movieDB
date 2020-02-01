1be9735cb69d53343f07f67785a6bb886a4e1427
Kimjisungs <kjsqwe9@gmail.com>
2020-02-01 22:40:25 +0900

    Feat: tab component, router.js data push in jsx

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -7,6 +7,13 @@ const HomePresenter = props => {
   return (
     <>
       <div>visual</div>
+      <Tab
+        to={[
+          { pathname: "/home", text: "무비" },
+          { pathname: "/home/tv", text: "티비" }
+        ]}
+      />
+      <Router />
     </>
   );
 };

d871dd25caf06db65fb07c029fc2e157350a419c
Kimjisungs <kjsqwe9@gmail.com>
2020-02-01 22:35:18 +0900

    Refactor: custome link '/' => '/home'

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -33,7 +33,7 @@ const SearchWrap = styled.div`
 export default () => (
   <Header>
     <Logo>
-      <Link to="/">
+      <Link to="/home">
         <img
           src="https://www.themoviedb.org/assets/2/v4/logos/primary-green-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.svg"
           alt="logo"

4f7453c6ed7a92b8fac193d0febcb4d10aa49422
Kimjisungs <kjsqwe9@gmail.com>
2020-02-02 14:38:16 +0900

    Feat: loading, error, popularlists update to state use try,catch,finally

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -3,6 +3,31 @@ import HomePresenter from "./HomePresenter";
 import { movieApi } from "../../api";
 
 class HomeContainer extends React.Component {
+  state = {
+    loading: true,
+    error: null,
+    popularLists: null
+  };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: popularLists }
+      } = await movieApi.popular();
+      this.setState({
+        popularLists
+      });
+    } catch {
+      this.setState({
+        error: "visual Not Found"
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
+
   render() {
     return (
       <>

404f4d32b4bec713bb36c55b69271990b3f9ec33
Kimjisungs <kjsqwe9@gmail.com>
2020-02-03 11:29:31 +0900

    Style: change font-size

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -10,7 +10,7 @@ const Input = styled.input`
   width: 100%;
   height: 100%;
   padding: 0 16px;
-  font-size: 30px;
+  font-size: 1.75rem;
   background-color: transparent;
   border: 4px solid #dfdfdf;
   color: #fff;

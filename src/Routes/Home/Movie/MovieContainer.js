5574dfd2c4bd3d2221bb7c9503bf9dce2d7fc3f9
Kimjisungs <kjsqwe9@gmail.com>
2020-02-03 13:51:29 +0900

    Feat: update object 'popular, torated, notheair' and delivery to MoviePresenter component

--- src/Routes/Home/Movie/MovieContainer.js
+++ src/Routes/Home/Movie/MovieContainer.js
@@ -1,16 +1,54 @@
 import React from "react";
 import MoviePresenter from "./MoviePresenter";
+import { movieApi } from "../../../api";
 
 class MovieContainer extends React.Component {
   state = {
     loading: null,
-    error: null
+    error: null,
+    popResults: null,
+    topResults: null,
+    onAirResults: null
   };
 
-  componentDidMount() {}
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: popResults }
+      } = await movieApi.popular();
+      const {
+        data: { results: topResults }
+      } = await movieApi.topRated();
+      const {
+        data: { results: onAirResults }
+      } = await movieApi.onTheAir();
+      this.setState({
+        popResults,
+        topResults,
+        onAirResults
+      });
+    } catch {
+      this.setState({
+        error: "Page Not Found"
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
 
   render() {
-    return <MoviePresenter />;
+    const { loading, error, popResults, topResults, onAirResults } = this.state;
+    return (
+      <MoviePresenter
+        loading={loading}
+        error={error}
+        popResults={popResults}
+        topResults={topResults}
+        onAirResults={onAirResults}
+      />
+    );
   }
 }
 

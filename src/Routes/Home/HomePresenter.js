d823057a9c2548e6529c55b517988235de921afc
Kimjisungs <kjsqwe9@gmail.com>
2020-02-03 11:34:37 +0900

    Feat: add tab title,active props and delete VisualContainer

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -21,37 +21,40 @@ const TabWrap = styled.div`
 const HomePresenter = ({ loading, error, popularLists }) => {
   return (
     <>
-      <VisualContainer>
-        {loading ? (
-          <div>로딩</div>
-        ) : (
-          <VisualWrap>
-            <Swiper>
-              {popularLists &&
-                popularLists.length > 0 &&
-                popularLists.map(visual => (
-                  <div key={visual.id}>
-                    <Visual
-                      id={visual.id}
-                      title={visual.name}
-                      image={visual.backdrop_path}
-                    />
-                  </div>
-                ))}
-            </Swiper>
-          </VisualWrap>
-        )}
-        <TabWrap>
-          <Tab
-            to={[
-              { pathname: "/home", text: "Movie" },
-              { pathname: "/home/tv", text: "TV" },
-              { pathname: "/home/People", text: "People" },
-              { pathname: "/home/genres", text: "genres" }
-            ]}
-          />
-        </TabWrap>
-      </VisualContainer>
+      {loading ? (
+        <div>로딩</div>
+      ) : (
+        <VisualWrap>
+          <Swiper>
+            {popularLists &&
+              popularLists.length > 0 &&
+              popularLists.map(visual => (
+                <div key={visual.id}>
+                  <Visual
+                    id={visual.id}
+                    title={visual.name}
+                    image={visual.backdrop_path}
+                  />
+                </div>
+              ))}
+          </Swiper>
+        </VisualWrap>
+      )}
+      <TabWrap>
+        <TabTitle>Movie Lists</TabTitle>
+        <Tabs
+          to={[
+            { pathname: "/home", text: "Movie" },
+            { pathname: "/home/tv", text: "TV" },
+            { pathname: "/home/People", text: "People" },
+            { pathname: "/home/genres", text: "genres" }
+          ]}
+          activeStyle={{
+            visible: `background-color: rgba(0, 0, 0, 0.3)`,
+            hidden: `background-color: transparent`
+          }}
+        />
+      </TabWrap>
       <Router />
     </>
   );

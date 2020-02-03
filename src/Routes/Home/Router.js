9440d7e9fc8ee13367469ac753483fc6852da6e1
Kimjisungs <kjsqwe9@gmail.com>
2020-02-03 12:34:33 +0900

    Refactor: delete genres path

--- src/Routes/Home/Router.js
+++ src/Routes/Home/Router.js
@@ -2,11 +2,11 @@ import React from "react";
 import { withRouter, Switch, Route, Redirect } from "react-router-dom";
 import Moive from "./Movie";
 import TV from "./TV";
+import People from "./People";
 
 export default withRouter(() => (
   <Switch>
-    <Route path="/home/genres" component={TV} />
-    <Route path="/home/People" component={TV} />
+    <Route path="/home/People" component={People} />
     <Route path="/home/tv" component={TV} />
     <Route path="/home" exact component={Moive} />
     <Redirect from="*" to="/home" />

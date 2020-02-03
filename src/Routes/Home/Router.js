1a7935c43fadb0d90393e5fdbf20bc677d545c25
Kimjisungs <kjsqwe9@gmail.com>
2020-02-03 10:54:48 +0900

    Feat: add path name in route

--- src/Routes/Home/Router.js
+++ src/Routes/Home/Router.js
@@ -5,6 +5,8 @@ import TV from "./TV";
 
 export default withRouter(() => (
   <Switch>
+    <Route path="/home/genres" component={TV} />
+    <Route path="/home/People" component={TV} />
     <Route path="/home/tv" component={TV} />
     <Route path="/home" exact component={Moive} />
     <Redirect from="*" to="/home" />

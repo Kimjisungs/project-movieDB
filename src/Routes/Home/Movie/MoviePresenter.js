9eeb5e7e7a4f57422d42cec26138804c65ea8be2
Kimjisungs <kjsqwe9@gmail.com>
2020-02-04 12:19:54 +0900

    Feat: process data and deliver it to 'poster'

--- src/Routes/Home/Movie/MoviePresenter.js
+++ src/Routes/Home/Movie/MoviePresenter.js
@@ -1,7 +1,63 @@
 import React from "react";
+import Section from "../../../Components/Section";
+import Loader from "../../../Components/Loader";
+import Poster from "../../../Components/Poster";
 
-const MoviePresenter = () => {
-  return <div>moviePresenter</div>;
+const MoviePresenter = ({
+  loading,
+  error,
+  popResults,
+  topResults,
+  onAirResults
+}) => {
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      {popResults && popResults.length > 0 && (
+        <Section title="Popular">
+          {popResults.map(movie => (
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              title={movie.name}
+              image={movie.poster_path}
+              date={movie.first_air_date}
+              overview={movie.overview}
+            />
+          ))}
+        </Section>
+      )}
+      {topResults && topResults.length > 0 && (
+        <Section title="Tob Rated">
+          {topResults.map(movie => (
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              title={movie.name}
+              image={movie.poster_path}
+              date={movie.first_air_date}
+              overview={movie.overview}
+            />
+          ))}
+        </Section>
+      )}
+      {onAirResults && onAirResults.length > 0 && (
+        <Section title="On The Air">
+          {onAirResults.map(movie => (
+            <Poster
+              key={movie.id}
+              id={movie.id}
+              title={movie.name}
+              image={movie.poster_path}
+              date={movie.first_air_date}
+              overview={movie.overview}
+            />
+          ))}
+        </Section>
+      )}
+    </>
+  );
 };
 
 export default MoviePresenter;

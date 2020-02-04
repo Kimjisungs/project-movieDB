d304a06ffab09def13969cedce0b18c2a370912f
Kimjisungs <kjsqwe9@gmail.com>
2020-02-04 14:03:01 +0900

    Style: add center style

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,8 +1,15 @@
 import React from "react";
+import styled from "styled-components";
+
+const Loading = styled.div`
+  text-align: center;
+`;
 
 export default () => (
-  <img
-    src="https://media.giphy.com/media/N256GFy1u6M6Y/giphy.gif"
-    alt="loading"
-  />
+  <Loading>
+    <img
+      src="https://media.giphy.com/media/N256GFy1u6M6Y/giphy.gif"
+      alt="loading"
+    />
+  </Loading>
 );

fa3e57fb8c8d87dfa6b5d8aae9c90e2801685a6d
Kimjisungs <kjsqwe9@gmail.com>
2020-02-04 11:58:27 +0900

    Feat: create poster and put in data

---
+++ src/Components/Poster.js
@@ -0,0 +1,89 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Poster = styled.li`
+  width: 20%;
+  padding: 0 10px;
+`;
+
+const Img = styled.div`
+  height: 320px;
+  overflow: hidden;
+  img {
+    width: 100%;
+  }
+  transition: opacity 0.3s linear;
+`;
+
+const Text = styled.div`
+  margin-top: 10px;
+  color: #dfdfdf;
+`;
+
+const Title = styled.h3`
+  font-size: 1.25rem;
+
+  overflow: hidden;
+  white-space: normal;
+  height: 1.2em;
+  line-height: 1.2;
+  word-wrap: break-word;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+`;
+
+const Date = styled.p`
+  margin-top: 10px;
+`;
+
+const Desc = styled.p`
+  margin-top: 10px;
+  overflow: hidden;
+  white-space: normal;
+  line-height: 1.2;
+  height: 3.6em;
+  font-size: 1rem;
+  font-weight: 300;
+  text-align: left;
+  word-wrap: break-word;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  opacity: 0.6;
+`;
+
+const Links = styled(Link)`
+  &:hover,
+  &:focus {
+    ${Img} {
+      opacity: 0.4;
+    }
+    ${Desc} {
+      text-decoration: underline;
+    }
+  }
+`;
+
+export default ({ id, title, image, date, overview }) => (
+  <Poster>
+    <Links>
+      <Img>
+        <img
+          src={
+            image
+              ? `https://image.tmdb.org/t/p/w500${image}`
+              : require("../assets/images/no_poster.jpg")
+          }
+          alt={title}
+        />
+      </Img>
+      <Text>
+        <Title>{title}</Title>
+        <Date>{date.substring(0, 4)}</Date>
+        <Desc>{overview}</Desc>
+      </Text>
+    </Links>
+  </Poster>
+);

97c1665acbf52fca895a2df239ecda355aa859f3
Kimjisungs <kjsqwe9@gmail.com>
2020-02-04 13:57:09 +0900

    Style: change message style

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -2,7 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 const Message = styled.p`
+  padding: 100px 0;
   font-size: 3rem;
+  color: #fff;
+  text-align: center;
 `;
 
 export default ({ text }) => <Message>{text}</Message>;

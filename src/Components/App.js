2f97a033e050962db6673bea8c59f8037be85f40
Kimjisungs <kjsqwe9@gmail.com>
2020-02-05 12:01:02 +0900

    Feat: Register 'Redux' in React

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Router from "./Router";
 import GlobalStyle from "./GrobalStyle";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import Reducers from "../Redux/Reducer";
 
 const App = () => {
   return (
-    <>
+    <Provider store={createStore(Reducers)}>
       <GlobalStyle />
       <Router />
-    </>
+    </Provider>
   );
 };
 

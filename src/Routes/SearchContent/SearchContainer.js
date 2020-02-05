76bd3466421060234dc655b34438de0784261311
Kimjisungs <kjsqwe9@gmail.com>
2020-02-05 17:03:59 +0900

    Feat: develop 'getApi' using searchQuery of uri

--- src/Routes/SearchContent/SearchContainer.js
+++ src/Routes/SearchContent/SearchContainer.js
@@ -1,8 +1,51 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { tvApi } from "../../api";
 import { connect } from "react-redux";
+import qs from "query-string";
 
 class SearchContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      error: null,
+      tvResults: null
+    };
+  }
+
+  uriQuery = () => {
+    const {
+      location: { search }
+    } = this.props;
+    const { query: uriQuery } = qs.parse(search);
+    return uriQuery;
+  };
+
+  getApi = async query => {
+    try {
+      const {
+        data: { results: tvResults }
+      } = await tvApi.searchTv(query);
+      this.setState({
+        tvResults
+      });
+    } catch {
+      this.setState({
+        error: "No Search Result"
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.getApi(this.uriQuery());
+  }
+
   render() {
     return (
       <>

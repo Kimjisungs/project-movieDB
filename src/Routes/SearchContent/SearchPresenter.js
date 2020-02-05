db8f4bf61af5fe7a395af67bd82f49e35b78804e
Kimjisungs <kjsqwe9@gmail.com>
2020-02-05 18:07:16 +0900

    Feat: put in data to SearchPresenter

--- src/Routes/SearchContent/SearchPresenter.js
+++ src/Routes/SearchContent/SearchPresenter.js
@@ -1,9 +1,31 @@
 import React from "react";
+import Section from "../../Components/Section";
+import Poster from "../../Components/Poster";
+import Loader from "../../Components/Loader";
 
-const SearchPresenter = () => {
+const SearchPresenter = ({ loading, error, tvResults }) => {
   return (
     <>
-      <div>22</div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {tvResults && tvResults.length > 0 && (
+            <Section title="TV Show">
+              {tvResults.map(tv => (
+                <Poster
+                  key={tv.id}
+                  id={tv.id}
+                  title={tv.name}
+                  image={tv.poster_path}
+                  date={tv.first_air_date}
+                  overview={tv.overview}
+                />
+              ))}
+            </Section>
+          )}
+        </>
+      )}
     </>
   );
 };

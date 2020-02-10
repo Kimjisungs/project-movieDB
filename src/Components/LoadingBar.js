86d63c17cf4f1166dd0b5098b48aa565a3894cb6
Kimjisungs <kjsqwe9@gmail.com>
2020-02-10 15:16:08 +0900

    Feat: develop loadingBar when page loading

---
+++ src/Components/LoadingBar.js
@@ -0,0 +1,92 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { loadingStart, loadingEnd } from "../Redux/Action";
+
+const AniWidthBar = keyframes`
+  0% {
+    width: 0;
+  }
+  100% {
+    width: 100%;
+  }
+`;
+
+const LoadingBarShape = styled.div`
+  z-index: 100;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 10px;
+  background-color: #eee;
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    animation: ${AniWidthBar} ${props => `${props.barAni * 0.001}`}s ease-in-out;
+    height: 100%;
+    background-color: red;
+    transition: all 0.2s;
+  }
+`;
+
+class LoadingBar extends React.Component {
+  barAni = () => {
+    const date = new Date();
+    const milliSeconds = date.getMilliseconds();
+    const {
+      loading,
+      startTime,
+      endTime,
+      loadingStart,
+      loadingEnd
+    } = this.props;
+    let totalSeconds = 0;
+
+    if (loading && !startTime) loadingStart(milliSeconds);
+    if (!loading && !endTime) loadingEnd(milliSeconds);
+
+    if (!loading && startTime && endTime) {
+      totalSeconds = Math.abs(startTime - endTime);
+      totalSeconds =
+        totalSeconds < 100 ? `0${totalSeconds}` : `${totalSeconds}`;
+      console.log(
+        `startTime : ${startTime}, endTime : ${endTime}, startTime - endTime: ${totalSeconds}`
+      );
+      console.log(`양수 totalSeconds : ${totalSeconds}`);
+      return totalSeconds;
+    }
+  };
+
+  componentWillUnmount() {
+    const { loadingStart, loadingEnd } = this.props;
+    loadingStart(0);
+    loadingEnd(0);
+  }
+
+  render() {
+    return <LoadingBarShape barAni={this.barAni()} />;
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    startTime: state.loading.milliSecond.start,
+    endTime: state.loading.milliSecond.end
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators(
+    {
+      loadingStart,
+      loadingEnd
+    },
+    dispatch
+  );
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoadingBar);

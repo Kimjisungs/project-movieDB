1c0de5f831f9bd35315a0bc00b98402720455334
Kimjisungs <kjsqwe9@gmail.com>
2020-02-12 08:15:37 +0900

    Feat: add search button because for a good 'UX'

--- src/Components/Search/SearchPresenter.js
+++ src/Components/Search/SearchPresenter.js
@@ -17,8 +17,34 @@ const Input = styled.input`
 `;
 
 const Form = styled.form`
+  position: relative;
   height: 100%;
 `;
+
+const Button = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 70px;
+  height: 100%;
+  border: 4px solid transparent;
+  background-color: #dfdfdf;
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    background-color: #a2a2a2;
+  }
+  &:active {
+    background-color: #b9b9b9;
+  }
+`;
+
+const searchStyle = {
+  pointerEvents: "none",
+  display: "block",
+  width: "100%",
+  height: "54%"
+};
 
 const SearchPresenter = ({ handleSubmit, updateQuery, searchQuery }) => {
   return (
@@ -29,6 +55,24 @@ const SearchPresenter = ({ handleSubmit, updateQuery, searchQuery }) => {
           onChange={updateQuery}
           value={searchQuery}
         />
+        <Button type="button" onClick={handleSubmit}>
+          <yt-icon class="style-scope ytd-searchbox">
+            <svg
+              viewBox="0 0 24 24"
+              preserveAspectRatio="xMidYMid meet"
+              focusable="false"
+              class="style-scope yt-icon"
+              style={searchStyle}
+            >
+              <g class="style-scope yt-icon">
+                <path
+                  d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
+                  class="style-scope yt-icon"
+                ></path>
+              </g>
+            </svg>
+          </yt-icon>
+        </Button>
       </Form>
     </SearchWrap>
   );

eb58d83193eb9a106b83b860d90b9113bbf9a398
Kimjisungs <kjsqwe9@gmail.com>
2020-02-14 08:57:39 +0900

    Style: add device response wrap layout UI style under 1024px

--- src/Components/Wrapper.js
+++ src/Components/Wrapper.js
@@ -3,11 +3,18 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   padding: 0 10px 100px;
+  @media all and (max-width: 1024px) {
+    padding: 0 0 50px;
+  }
 `;
 
 const Container = styled.div`
   width: 1200px;
   margin: 0 auto;
+  @media all and (max-width: 1024px) {
+    width: 100%;
+    margin: 0;
+  }
 `;
 
 export default ({ children }) => {
--- src/Components/WrapperContent.js
+++ src/Components/WrapperContent.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 
 const WrapperContent = styled.div`
   min-height 600px;
+  @media all and (max-width: 1024px) {
+    padding: 0 20px;
+  }
 `;
 
 export default ({ children }) => <WrapperContent>{children}</WrapperContent>;
